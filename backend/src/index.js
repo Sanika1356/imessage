ae983c1af5c0a77207a4fe15c3c634854c5f9781
Sanika1356 <sanikasp135@gmail.com>
2026-06-14 13:10:58 +0530

    feat: add cron job to send health check requests every 14 minutes
    - Added "cron" dependency to package.json.
    - Implemented a cron job in a new cron.js file that sends a GET request to the health endpoint every 14 minutes.
    - Updated index.js to start the cron job in production environment.

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,4 +40,7 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is up and running on PORT:", PORT);
+
+
+    if (process.env.NODE_ENV === "production") job.start();
 });
